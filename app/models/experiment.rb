52fd32b3b21ddb933f0f756a40de9d17c022c6bf
Robin Meyers <robin.m.meyers@gmail.com>
2013-09-19 19:13:58 -0400

    length validation on experiment name

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -5,7 +5,7 @@ class Experiment < ActiveRecord::Base
 
   default_scope -> { joins(:sequencing).order('sequencings.completed_on DESC') }
 
-  validates :name, presence: true
+  validates :name, presence: true, length: {maximum: 50}
   validates :researcher_id, presence: true
   validates :sequencing_id, presence: true
 end
