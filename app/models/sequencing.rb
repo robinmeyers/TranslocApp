cd14a73d4d50e32ad31c44523c1c8b195c09294c
Robin Meyers <robin.m.meyers@gmail.com>
2013-09-19 16:34:57 -0400

    refactored (un)completed sequencing runs using scoping on the model

--- app/models/sequencing.rb
+++ app/models/sequencing.rb
@@ -1,6 +1,8 @@
 class Sequencing < ActiveRecord::Base
 
   default_scope -> { order('completed_on DESC') }
+  scope :uncompleted, -> { where(completed_on: nil) }
+  scope :completed, -> { where.not(completed_on: nil) }
   validates :run, presence: true, uniqueness: { case_sensitive: false }
 
   self.per_page = 10
